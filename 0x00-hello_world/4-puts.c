f7bc43861f7cac15e750fb5a9f0550358d9affa4
Okpara Emmanuel Oluchi <chiedooluchi2017@gmail.com>
2023-03-16 17:41:52 -0700

    Commit puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * Description: main - prints out a string to stdout.
- * Return: 0 if Success.
+ * main - a program that prints a line with puts function
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }

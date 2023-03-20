5c4d7d02c30bd35892a31e76fb043de05195a212
Okpara Emmanuel Oluchi <chiedooluchi2017@gmail.com>
2023-03-20 00:43:39 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,11 +11,14 @@ int main(void)
 int num;
 char letter;
 
-for (num = 0; num < 10; num++)
-putchar((num % 10) + 'o');
-
+for (num = 0; num <= '9'; num++)
+{
+putchar(num);
+}
 for (letter = 'a'; letter <= 'f'; letter++)
+{
 putchar(letter);
+}
 
 putchar('\n');
 

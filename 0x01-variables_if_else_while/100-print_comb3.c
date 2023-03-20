2ff3536c0602376370e1de39cfd2dbbd4727993b
Okpara Emmanuel Oluchi <chiedooluchi2017@gmail.com>
2023-03-20 10:50:47 -0700

    commit to c program

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,32 +6,32 @@
  */
 int main(void)
 {
-        int a = 0;
-        int b = 0;
-        int c = 0;
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	while (a <= 9)
+	{
+	c++;
+	b = c;
+	while (b <= 9)
+	{
+	putchar('0' + a);
+	putchar('0' + b);
+	if (a == 8 && b == 9)
+	{
 
-        while (a <= 9)
-        {
-        c++;
-        b = c;
-        while (b <= 9)
-        {
-        putchar('0' + a);
-        putchar('0' + b);
-        if (a == 8 && b == 9)
-        {
-
-        }
-        else
-        {
-        putchar(',');
-        putchar(' ');
-        }
-        b++;                            }
-        b = 0;
-        a++;
-        }
-        putchar('\n');
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	b++;
+	}		
+	b = 0;
+	a++;
+	}
+putchar('\n');
 return (0);
 }
 

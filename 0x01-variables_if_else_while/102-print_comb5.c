f502a43e03028b99355f521cb6687cce6bf24366
Okpara Emmanuel Oluchi <chiedooluchi2017@gmail.com>
2023-03-20 11:01:06 -0700

    commit to c program

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,32 +4,25 @@
  *
  * Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
-int a = 0;
-int q = 0;
-for (a = 0; a <= 98; a++)
+int p, q;
+for (p = 0; p <= 98; p++)
 {
-for (q = a + 1; q <= 99; q++)
+for (q = p + 1; q <= 99; q++)
 {
-putchar((a / 10) + '0');
-putchar((a % 10) + '0');
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
 putchar(' ');
 putchar((q / 10) + '0');
 putchar((q % 10) + '0');
-if (a == 98 && q == 99);
+if (p == 98 && q == 99)
 continue;
 putchar(',');
 putchar(' ');
 }
-q++;
-}
-q = 0;
-a++;
-}
-a = 0;
 }
 putchar('\n');
 return (0);
 }
+

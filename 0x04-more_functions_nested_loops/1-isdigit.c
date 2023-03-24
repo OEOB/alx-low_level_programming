5d8ffaf2799a52738cac1aebc813b1148ff5944b
Okpara Emmanuel Oluchi <chiedooluchi2017@gmail.com>
2023-03-24 00:41:31 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,16 +4,13 @@
 /**
  * _isdigit  - Checks for a digit
  * @c: The character to be checked
- * Return: 1 for digit character or 0 for anything else
+ * Return: 1 if c is a digit, 0 otherwise
  */
-
 int _isdigit(int c)
+{
 
-{
-if (c >= 43 && c <= 57)
-{
+if ((c >= 48) && (c <= 57))
 return (1);
-}
 else
 return (0);
 }

2c3d8e910b8994fe61b885c5a0c499d0088f4709
Okpara Emmanuel Oluchi <chiedooluchi2017@gmail.com>
2023-04-07 10:17:53 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,44 +4,29 @@
 /**
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: The number to start printing from
- * Return: Always 0.
  */
-
-void print_times_table(int n)
-
+void print_to_98(int n)
 {
-
 if (n <= 98)
 {
-for (!n <= 98; n++)
+while (n <= 98)
 {
 if (n == 98)
-{
-printf("%d", n);
-printf("\n");
-break;
-}
+printf("%d\n", n);
 else
-{
 printf("%d, ", n);
+n = n + 1;
 }
 }
-}
-else
+else if (n > 98)
 {
-for (; n >= 95; n--)
+while (n >= 98)
 {
 if (n == 98)
-{
-printf("%d", n);
-printf("\n");
-break;
-}
+printf("%d\n", n);
 else
-{
 printf("%d, ", n);
+n = n - 1;
 }
 }
-}
-
 }

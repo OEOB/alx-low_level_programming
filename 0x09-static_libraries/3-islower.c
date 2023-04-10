f44fda6d67053bc5113ab5b6ffa5e87fd669aaf0
Okpara Emmanuel Oluchi <chiedooluchi2017@gmail.com>
2023-04-10 02:53:09 -0700

    tial

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - function to check mfor lowercase character
+ * @c: id the int that will be used for the argument of the function
+ * Return: o
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}
+
